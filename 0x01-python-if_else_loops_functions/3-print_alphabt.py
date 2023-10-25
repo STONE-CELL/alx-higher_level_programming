74a6b46edaace56a2b178e9073c38ec6c09a7586
STONE-CELL <mohamedelkasir237682511@gmail.com>
2023-10-25 04:54:23 -0700

    uyt

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for l in range(97, 123):
-    if chr(l) is not 'q' and chr(l) is not 'e':
-        print("{}".format(chr(l)), end="")
-
+for i in range(ord('a'), ord('z') + 1):
+    if i != ord('q') and i != ord('e'):
+        print("{:c}".format(i), end="")
