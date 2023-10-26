09bd328f8725ee93b2bf9f4d1dc77c0cbc261092
STONE-CELL <mohamedelkasir237682511@gmail.com>
2023-10-25 21:58:51 -0700

    uyt

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""Square module"""
 class Square:
+    """Define a square"""
+
     def __init__(self, size):
+        """constructor
+
+        Args:
+            size: length of a side of square
+        """
         self.__size = size
