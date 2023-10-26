defc37952da38070c30217b22194004388a5a2f6
STONE-CELL <mohamedelkasir237682511@gmail.com>
2023-10-25 23:19:37 -0700

    uyt

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,13 +2,5 @@
 def weight_average(my_list=[]):
     if not my_list:
         return 0
-
-    ser = 0
-    sey = 0
-
-    for tup in my_list:
-        num += tup[0] * tup[1]
-        sey += tup[1]
-
-    return (ser / sey)
-
+    else:
+        return(sum(a * b for a, b in my_list) / sum(b for a, b in my_list)
