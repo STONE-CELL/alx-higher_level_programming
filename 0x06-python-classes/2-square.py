6d302b15749468907e03e69301f2a5735d256269
STONE-CELL <mohamedelkasir237682511@gmail.com>
2023-10-25 22:07:41 -0700

    uyt

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""Square module"""
+"""Square module."""
 
 class Square:
-    """Defines a square"""
+    """Defines a square."""
 
     def __init__(self, size=0)
-        """constructor
+        """Constructor.
 
         Args:
-            size: length of a side of square
+            size: Length of a side of square.
 
         Raises:
-            TypeError: if size is not an integer
-            ValueError: if size is less than 0
+            TypeError: If size is not an integer
+            ValueError: If size is less than 0
         """
 
     if not isinstance(size, int):
